59106724865925a74522e4e5596c56f78589cc38
gangaprasadmohite <gemohite@gmail.com>
2022-03-04 23:51:32 +0530

    Added conditional styling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import Card from "./shared/Card"
+import PropTypes from 'prop-types'
 
-function FeedbackItem({item}) {
-    
-    
+function FeedbackItem({item}) { 
   return (
-    <div className='card'>
+    <Card>
          <div className='num-display'>
             {item.rating}
          </div>
          <div className='text-display'>
             {item.text}
          </div>
-    </div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired 
 }
 
 export default FeedbackItem
