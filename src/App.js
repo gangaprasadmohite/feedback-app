25e1f5817ccafb50edbe47455b2ce53924bd880e
gangaprasadmohite <gemohite@gmail.com>
2022-03-05 23:46:21 +0530

    Added Feedback stats

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -15,6 +16,7 @@ function App() {
     <React.Fragment>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </React.Fragment>
