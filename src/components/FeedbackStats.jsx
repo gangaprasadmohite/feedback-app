dbda8d0860ddeb918abf896ff93a2aa717c0493b
gangaprasadmohite <gemohite@gmail.com>
2022-03-05 23:57:13 +0530

    Added Feedback form

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -8,7 +8,7 @@ function FeedbackStats({feedback}) {
     average = average.toFixed(1).replace(/[.,]0$/,'')
   return (
     <div className='feedback-stats'>
-        <h4>{feedback.length}Reviews</h4>
+        <h4>{feedback.length} Reviews</h4>
         <h4>Average rating:{ isNaN(average) ? 0 : average  }</h4>
     </div>
   )
