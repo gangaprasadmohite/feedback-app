d4eee6952af17d2308a27116db29a01cf6efa238
gangaprasadmohite <gemohite@gmail.com>
2022-03-06 21:23:47 +0530

    Added real-time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,19 @@
  
  function FeedbackForm() {
      const [text, setText] = useState('')
+     const [btnDisabled, setBtnDisabled] = useState(true)
+     const [message, setMessage] = useState('')
      const handleTextChange = (e) => {
+         if(text === '') {
+             setBtnDisabled(true)
+             setMessage(null)
+         } else if(text !=='' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage("Text must be atleast 10 characters.")
+         } else {
+             setMessage(null)
+             setBtnDisabled(false)
+         }
           setText(e.target.value)
      }
    return (
@@ -15,8 +27,9 @@
                 {}
                 <div className='input-group'>
                     <input onChange={handleTextChange} value={text  } type="text" placeholder='Write a review'/>
-                    <Button type='submit'></Button>
+                    <Button type='submit' isDisabled={btnDisabled}>Send</Button>
                 </div>
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     )
