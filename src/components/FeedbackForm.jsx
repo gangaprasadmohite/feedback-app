4f5657e0144a01b394e3e6b945a62e96fa127db9
gangaprasadmohite <gemohite@gmail.com>
2022-03-07 21:56:43 +0530

    Handled side effects with useEffect

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,25 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
 import RatingSelect from './RatingSelect';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackForm({}) {
-  const { addFeedback } = useContext(FeedbackContext);
-
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
+
+  const { addFeedback, feedbackEdit } = useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
+
   const handleTextChange = (e) => {
     if (text === '') {
       setBtnDisabled(true);
